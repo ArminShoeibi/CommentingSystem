fcabbfdf600c2b380287a1713a7519bb56e8c75a
Sajad Afaghiy <sajad.afaghiy@gmail.com>
2020-11-13 23:39:47 +0330

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,70 +19,39 @@ namespace CommentingSystem.Controllers
             _db = db;
         }
 
-
         public async Task<IActionResult> Index()
         {
 
-            List<Comment> comments = await _db.Comments.AsNoTrackingWithIdentityResolution()
-                                                        .Include(c => c.Children)
-                                                        .ToListAsync();
+            List<Comment> comments = await _db.Comments
+                .AsNoTrackingWithIdentityResolution()
+                .Include(c => c.Children)
+                .ToListAsync();
 
-            // Structure Comments into a tree
-            List<Comment> commentsTree = comments.Where(c => c.ParentId == null)
-                                                 .AsParallel()
-                                                 .ToList();
+            // Structure comments into a tree
+            List<Comment> rootComments = comments
+                .Where(c => c.ParentId == null)
+                .AsParallel()
+                .ToList();
 
-            return View(commentsTree);
+            return View(rootComments);
         }
-
 
         [HttpPost]
         public async Task<IActionResult> CreateComment(CommentCreationDto commentCDto)
         {
             if (ModelState.IsValid)
             {
-                Comment newComment = new()
+                var newComment = new Comment
                 {
-                    DateCreated = DateTimeOffset.Now,
-                    DateModified = DateTimeOffset.Now,
-                    Content = commentCDto.Content,
+                    ParentId = commentCDto.ReplyToCommentId,
                     FullName = commentCDto.FullName,
                     Email = commentCDto.Email,
+                    Content = commentCDto.Content,
+                    DateCreated = DateTimeOffset.Now,
+                    DateModified = DateTimeOffset.Now
                 };
 
                 await _db.Comments.AddAsync(newComment);
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            // TODO: Throw some error
-            return RedirectToAction(nameof(Index));
-
-
-        }
-
-
-        [HttpGet]
-        public IActionResult CreateReply(int replyToCommentId)
-        {
-            return PartialView(new CommentReplyDto() { ReplyToCommentId = replyToCommentId });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateReply(CommentReplyDto commentRDto)
-        {
-            if (ModelState.IsValid)
-            {
-                Comment newReply = new()
-                {
-                    DateCreated = DateTimeOffset.Now,
-                    DateModified = DateTimeOffset.Now,
-                    ParentId = commentRDto.ReplyToCommentId,
-                    Email = commentRDto.Email,
-                    FullName = commentRDto.FullName,
-                    Content = commentRDto.Content,
-                };
-                await _db.Comments.AddAsync(newReply);
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
