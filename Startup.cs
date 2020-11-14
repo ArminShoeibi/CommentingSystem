41e3aa168e234b19137ba90c7191354ad99cc42a
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2020-11-14 19:51:11 +0330

    Remove unnecessary usings

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
 using CommentingSystem.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CommentingSystem
 {
