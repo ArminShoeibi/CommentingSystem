c0192a2be19347868f87e053f5c238e2f77ea4d2
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2021-04-09 01:51:11 +0430

    Change form actions and  Dto that creates a comment.

---
+++ DTOs/CreateCommentDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommentingSystem.DTOs
+{
+    public record CreateCommentDto
+    {
+        public int? ReplyToCommentId { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(1000, MinimumLength = 10)]
+        public string Content { get; init; }
+
+        [EmailAddress]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 6)]
+        public string Email { get; init; }
+
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(60, MinimumLength = 3)]
+        public string FullName { get; init; }
+    }
+}
