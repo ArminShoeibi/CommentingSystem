4175315ad6b579c192e2c4a078bd9265c2e648d0
armin <a.shoeibi@alibaba.ir>
2022-04-14 04:07:13 +0430

    WIP

--- src/CommentingSystem.Web/DTOs/CreateCommentDto.cs
+++ src/CommentingSystem.Web/DTOs/CreateCommentDto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommentingSystem.DTOs;
 public record class CreateCommentDto
@@ -11,7 +12,7 @@ public record class CreateCommentDto
     [EmailAddress, StringLength(100, MinimumLength = 6), Required]
     public string Email { get; init; }
 
-    [Required, StringLength(60, MinimumLength = 3)]
+    [Required, StringLength(60, MinimumLength = 3),DisplayName("Full Name")]
     public string FullName { get; init; }
 }
 
